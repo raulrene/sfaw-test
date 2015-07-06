1b5bb1042ff347332380fbb5060b9fd479e4e72c
pelivansky <cpelivan30@yahoo.com>
2015-07-06 17:09:47 +0300

    head modifs

--- tf_header/head.php
+++ tf_header/head.php
@@ -5,6 +5,10 @@
 	<title>Academy-Learning</title>
 	<link rel="stylesheet" href="style.css" type="text/css">
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">	
+	<link href="index.css" type="text/css" rel="stylesheet">
+    <link href="carouselSlider/slider.css" type="text/css" rel="stylesheet">
+    <link href="main-content/main-content.css" type="text/css" rel="stylesheet">
+    <link href="footer/footer.css" type="text/css" rel="stylesheet">	
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
