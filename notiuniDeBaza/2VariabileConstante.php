30678a49b4f92cd5caf6ab1a15ecd304aec1b436
Dan <dan.lepsa94@gmail.com>
2015-07-06 13:30:34 +0300

    2VariabileCOnstante

---
+++ notiuniDeBaza/2VariabileConstante.php
@@ -0,0 +1,35 @@
+<?php
+// variabile numerice
+$variabila = 1;
+$numar = 0;
+ 
+// variabile text (stringuri)
+$text = "Salut";
+$text2 = 'Ce faci?';
+ 
+// variabile logice (boolean)
+$stiuPHP = true;
+$uitRepede = false;
+ 
+// vector
+$vectorGol = array();
+$vectorS = array( 10,11,12,13 ); // vector simplu
+ 
+// vector asociativ
+$vector = array( 
+	'luni' => 'Monday', 
+	'marti' => 'Tuesday',
+	'miercuri' => 'Wednesday'
+);
+ 
+// definirea unei noi componente a unui vector asociativ
+$vector[ 'joi' ] = 'Thursday';
+ 
+// vector simplu definit ca asociativ, echivalentul lui $vectorS
+$vectorS2 = array(
+	0 => 10,
+	1 => 11,
+	2 => 12,
+	3 => 13
+);
+?>
