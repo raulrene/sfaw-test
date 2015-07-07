c2817bbfef0e240bc88e2e83ab6975a9fb03564b
Radu Spinean <radu.spinean@yahoo.ro>
2015-07-07 12:07:17 +0300

    Capitolul 1 complet

---
+++ notiuni_de_baza/variabile_constante.php
@@ -0,0 +1,27 @@
+<?php
+$variabila = 1;
+$numar = 0;
+
+$text = "Salut";
+$text2 = 'Ce faci?';
+
+$stiuPHP = true;
+$uitRepede = false;
+
+$vectorGol = array();
+$vectorS = array( 10,11,12,13 ); // vector simplu
+
+$vector = array(
+    'luni' => 'Monday',
+    'marti' => 'Tuesday',
+    'miercuri' => 'Wednesday'
+);
+
+$vector[ 'joi' ] = 'Thursday';
+
+$vectorS2 = array(
+    0 => 10,
+    1 => 11,
+    2 => 12,
+    3 => 13
+);
