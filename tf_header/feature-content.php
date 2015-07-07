92120c667cb588d1cb715f86b573f4048bbe676f
Dan <dan.lepsa94@gmail.com>
2015-07-07 11:44:47 +0300

    aaa

---
+++ tf_header/feature-content.php
@@ -0,0 +1,60 @@
+<div class="site-wrap">
+    <div class="featured-content">
+
+        <div class="row">
+            <div class="fivecol column" style="top: 150.5px;">
+                <h1>Learning Online Becomes Easier</h1>
+                <p>
+Academy is a perfect theme for sharing and selling your knowledge online. It’s not just a theme, but learning management system that provides great features to make learning and teaching online easier for everyone.
+                </p>
+
+                <a href=" " target="_self" class="button large primary">
+                    <img class="alignnone" alt="" src="carouselSlider/img/bookImg.png">
+
+    Get Started
+
+</a>
+            </div>
+
+            <div class="sevencol column last">
+                <img class="alignnone" alt="" src="carouselSlider/img/slide_1.png">
+            </div>
+        </div>
+
+        <div class="arrow arrow-left"></div>
+        <div class="arrow arrow-right"></div>
+    </div>
+    <div class="main-content">
+
+        <div class="row">
+
+            <div class="fourcol column">
+                <div class="three-col">
+                    <img class="alignnone float-left" alt="" src="main-content/img/book.jpg">
+                    <h3>Learn anything online</h3>
+                </div>
+                <div class="clear"></div>
+                <p>Suspendisse ante mi, iaculis ac eleifend id, venenatis non eros. Sed rhoncus gravida elit, eu sollicitudin sem iaculis. Proin scelerisque, ipsum mollis posuere metus.</p>
+
+            </div>
+
+            <div class="fourcol column">
+                <div class="three-col">
+                    <img class="alignnone float-left" alt="" src="main-content/img/book.jpg">
+                    <h3>Talk to Our Experts</h3>
+                </div>
+                <div class="clear"></div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna mauris, sagittis tempus varius vel, eleifend ut metus. Pellentesque sapien tellus, accumsan.</p>
+
+            </div>
+
+            <div class="fourcol column last">
+                <div class="three-col">
+                    <img class="alignnone float-left" alt="" src="main-content/img/book.jpg">
+                    <h3>Communicate with People</h3>
+                </div>
+                <div class="clear"></div>
+                <p>Nullam viverra, magna vitae mollis venenatis, leo mi dignissim quam, et lacinia purus diam nec sapien. Cras aliquet, purus non rhoncus aliquet, dapibus.</p>
+
+
+            </div>
---
+++ tf_header/includes.php
@@ -0,0 +1,9 @@
+<?php
+include("head.php");
+include('header.php');
+include('feature-content.php');
+include('courses.php');
+include('main-content.php');
+include('footer.php');
+
+?>
