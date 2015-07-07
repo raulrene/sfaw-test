362c1727d96721bcac7cd2c0231cd4bf1b7eea4a
Radu Spinean <radu.spinean@yahoo.ro>
2015-07-07 12:02:33 +0300

    capitol 1 complet

---
+++ notiuni_de_baza/notiuni_baza.php
@@ -0,0 +1,34 @@
+<?php
+
+print "text";
+echo "text";
+echo "<br />", "Afisez", " un text din ", 4, " bucati";
+print "<br />";
+print "Afisez";
+print " un text din ";
+print 4;
+print  " bucati";
+printf( "<br />Am %4.2f lei", 102.123456 );
+print "<br />";
+print "Salut";
+print "Acesta este un script simplu";
+print "<br />";
+print "Salut";
+print "\n";
+print "<br />";
+print "Acesta este un script simplu";
+print "<br />";
+print 1;
+print 2;
+print 3;
+print "<br />";
+print "Salut, straine! ";
+print "<br />";
+print 'Salut din nou! ';
+print "<br />";
+print <<<SIR
+Salut iar!
+SIR;
+print "<br />";
+print /* partea asta e ignorata */ "Salut!";
+?>
