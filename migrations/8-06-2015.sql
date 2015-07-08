17208786de725c4bc1d480fd79f48916e8ecdc43
Dan <dan.lepsa94@gmail.com>
2015-07-08 15:28:55 +0300

    created table SubCapitol

--- migrations/8-06-2015.sql
+++ migrations/8-06-2015.sql
@@ -12,3 +12,15 @@ CREATE TABLE `siteDB`.`Capitol` (
   `idCapitol` INT NOT NULL AUTO_INCREMENT,
   `numeCapitol` VARCHAR(1500) NOT NULL,
   PRIMARY KEY (`idCapitol`));
+
+CREATE TABLE `siteDB`.`SubCapitol` (
+  `idSubCapitol` INT NOT NULL AUTO_INCREMENT,
+  `idParentCapitol` INT NOT NULL,
+  `numeSubCapitol` VARCHAR(1500) NOT NULL,
+  PRIMARY KEY (`idSubCapitol`),
+  INDEX `fk_subcapCapitol_idx` (`idParentCapitol` ASC),
+  CONSTRAINT `fk_subcapCapitol`
+    FOREIGN KEY (`idParentCapitol`)
+    REFERENCES `siteDB`.`Capitol` (`idCapitol`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
