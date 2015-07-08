9eb21c489287cbfc46a866c57faed9392c6802c8
Radu Spinean <radu.spinean@yahoo.ro>
2015-07-09 00:09:57 +0300

    modificare id capitol

--- migrations/08.07.2015.sql
+++ migrations/08.07.2015.sql
@@ -1,14 +1,20 @@
 CREATE SCHEMA `primarydb` ;
+
 CREATE TABLE `primarydb`.`configurare` (
   `ID` INT NOT NULL AUTO_INCREMENT COMMENT '',
   `logo` VARCHAR(1500) NULL COMMENT '',
   `background` VARCHAR(1500) NULL COMMENT '',
   PRIMARY KEY (`ID`)  COMMENT '');
+
 CREATE TABLE `primarydb`.`capitol` (
   `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
   `capitol` VARCHAR(255) NULL COMMENT '',
   PRIMARY KEY (`id`)  COMMENT '');
+
 CREATE TABLE `primarydb`.`subcapitol` (
   `idsubcapitol` INT NOT NULL AUTO_INCREMENT COMMENT '',
   `subcapitol` VARCHAR(255) NULL COMMENT '',
-  PRIMARY KEY (`idsubcapitol`)  COMMENT '');git@github.com:raulrene/sfaw-test.git
+  PRIMARY KEY (`idsubcapitol`)  COMMENT '');
+
+ALTER TABLE `primarydb`.`capitol`
+CHANGE COLUMN `id` `idcapitol` INT(11) NOT NULL COMMENT '' ;
