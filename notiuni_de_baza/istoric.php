b959a6a468dffe51d99766501b56965b415e8512
Radu Spinean <radu.spinean@yahoo.ro>
2015-07-08 13:22:31 +0300

    examples of search history

---
+++ notiuni_de_baza/istoric.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+?>
+
+<form method="post" action="">
+    Cuvant cheie <input type="text" name="keyword" value="" />
+    <input type="submit" name="trimite" value="Cauta" />
+    <input type="reset" value="Curata" />
+</form>
+
+<?php
+    if( isset( $_SESSION['cautari'] ) ) {
+$istoric = $_SESSION['cautari'];
+} else {
+$istoric = array();
+}
+
+
+if( isset($_POST) && !empty($_POST['keyword']) ) {
+
+array_push($istoric, $_POST['keyword']);
+
+
+$_SESSION['cautari'] = $istoric;
+
+
+}
+
+if( empty( $istoric ) ) {
+    echo 'Nu ati cautat nimic pana acum';
+
+} else {
+
+
+    echo '<ul>';
+
+
+    foreach($istoric as $termen) {
+        echo "<li>$termen</li>";
+    }
+
+  
+    echo '</ul><p>', 'Cautari efectuate: ', count( $istoric ), '</p>';
+}
+?>
