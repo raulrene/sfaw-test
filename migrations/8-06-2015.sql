eaecdcae58bb213788aaed0a190acbb1140d68b5
Dan <dan.lepsa94@gmail.com>
2015-07-08 15:12:41 +0300

    created Config  table

--- migrations/8-06-2015.sql
+++ migrations/8-06-2015.sql
@@ -1,1 +1,8 @@
 CREATE SCHEMA 'siteDB';
+
+CREATE TABLE `siteDB`.`Configurare` (
+  `idConfigurare` INT NULL AUTO_INCREMENT,
+  `logo` VARCHAR(1500) NULL,
+  `background` VARCHAR(1500) NULL,
+  `font-family` VARCHAR(1500) NULL,
+  PRIMARY KEY (`idConfigurare`));
