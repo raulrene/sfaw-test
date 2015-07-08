72cd3b47b706fc43a9efd84d218f95caf5896b5c
Radu Spinean <radu.spinean@yahoo.ro>
2015-07-08 15:53:37 +0300

    creare tabel configurare

--- migrations/08.07.2015.sql
+++ migrations/08.07.2015.sql
@@ -1,1 +1,6 @@
 CREATE SCHEMA `primarydb` ;
+CREATE TABLE `primarydb`.`configurare` (
+  `ID` INT NOT NULL AUTO_INCREMENT COMMENT '',
+  `logo` VARCHAR(1500) NULL COMMENT '',
+  `background` VARCHAR(1500) NULL COMMENT '',
+  PRIMARY KEY (`ID`)  COMMENT '');
