3cd0f1d7d0003bdfaecfaaf6de915f1bffb63c6d
pelivansky <cpelivan30@yahoo.com>
2015-07-08 15:14:54 +0300

    create config table

--- migrations/08-06-2015.sql
+++ migrations/08-06-2015.sql
@@ -1,1 +1,8 @@
 CREATE SCHEMA `site` ;
+CREATE TABLE `site`.`config` (
+  `idconfig` INT NULL AUTO_INCREMENT,
+  `site-logo` VARCHAR(1500) NULL,
+  `site-background` VARCHAR(1500) NULL,
+  `site-fontFam` VARCHAR(1500) NULL,
+  `site-fontSize` VARCHAR(1500) NULL,
+  PRIMARY KEY (`idconfig`));
