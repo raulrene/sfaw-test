4934878d087969fb61fcdfbad5c2ef13097a239e
Radu Spinean <radu.spinean@yahoo.ro>
2015-07-09 13:10:19 +0300

    clasa capitol

---
+++ classes/capitol.php
@@ -0,0 +1,56 @@
+<?php
+
+
+class capitol {
+    private $id;
+    private $capitol;
+    private $order;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCapitol()
+    {
+        return $this->capitol;
+    }
+
+    /**
+     * @param mixed $capitol
+     */
+    public function setCapitol($capitol)
+    {
+        $this->capitol = $capitol;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOrder()
+    {
+        return $this->order;
+    }
+
+    /**
+     * @param mixed $order
+     */
+    public function setOrder($order)
+    {
+        $this->order = $order;
+    }
+}
