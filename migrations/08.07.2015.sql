c9687a80030d9eee4d84f629cbaa3f2617ec48fc
Radu Spinean <radu.spinean@yahoo.ro>
2015-07-09 11:10:24 +0300

    modificari

--- migrations/08.07.2015.sql
+++ migrations/08.07.2015.sql
@@ -50,3 +50,105 @@ CREATE TABLE `primarydb`.`content` (
   REFERENCES `primarydb`.`subsubcapitol` (`idsubsubcapitol`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
+
+ALTER TABLE `primarydb`.`subsubcapitol`
+DROP FOREIGN KEY `key 2`;
+
+ALTER TABLE `primarydb`.`subcapitol`
+DROP FOREIGN KEY `key1`;
+
+ALTER TABLE `primarydb`.`content`
+DROP FOREIGN KEY `key3`;
+
+ALTER TABLE `primarydb`.`capitol`
+CHANGE COLUMN `idcapitol` `id` INT(11) NOT NULL COMMENT '' ;
+
+ALTER TABLE `primarydb`.`subcapitol`
+CHANGE COLUMN `idsubcapitol` `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '' ,
+CHANGE COLUMN `idcapitol` `capitol` INT(11) NULL COMMENT '' ;
+ALTER TABLE `primarydb`.`subcapitol`
+ADD CONSTRAINT `capitol`
+FOREIGN KEY (`capitol`)
+REFERENCES `primarydb`.`capitol` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+ALTER TABLE `primarydb`.`subcapitol`
+CHANGE COLUMN `subcapitol` `sub_capitol` VARCHAR(255) NULL DEFAULT NULL COMMENT '' ;
+
+ALTER TABLE `primarydb`.`subsubcapitol`
+CHANGE COLUMN `idsubsubcapitol` `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '' ,
+CHANGE COLUMN `subsubcapitol` `sub_sub_capitol` VARCHAR(45) NULL COMMENT '' ,
+CHANGE COLUMN `idsubcapitol` `subcapitol_id` INT(11) NOT NULL COMMENT '' ;
+ALTER TABLE `primarydb`.`subsubcapitol`
+ADD CONSTRAINT `subcapitol`
+FOREIGN KEY (`subcapitol_id`)
+REFERENCES `primarydb`.`subcapitol` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `primarydb`.`subcapitol`
+DROP FOREIGN KEY `capitol`;
+ALTER TABLE `primarydb`.`subcapitol`
+CHANGE COLUMN `capitol` `capitol_id` INT(11) NULL DEFAULT NULL COMMENT '' ;
+ALTER TABLE `primarydb`.`subcapitol`
+ADD CONSTRAINT `capitol_id`
+FOREIGN KEY (`capitol_id`)
+REFERENCES `primarydb`.`capitol` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `primarydb`.`subsubcapitol`
+DROP FOREIGN KEY `subcapitol`;
+ALTER TABLE `primarydb`.`subsubcapitol`
+ADD CONSTRAINT `subcapitol_id`
+FOREIGN KEY (`subcapitol_id`)
+REFERENCES `primarydb`.`subcapitol` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `primarydb`.`content`
+CHANGE COLUMN `idcontent` `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '' ,
+CHANGE COLUMN `idsubsubcapitol` `sub_sub_capitol_id` INT(11) NOT NULL COMMENT '' ;
+ALTER TABLE `primarydb`.`content`
+ADD CONSTRAINT `sub_sub_capitol_id`
+FOREIGN KEY (`sub_sub_capitol_id`)
+REFERENCES `primarydb`.`subsubcapitol` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `primarydb`.`subsubcapitol`
+DROP FOREIGN KEY `subcapitol_id`;
+ALTER TABLE `primarydb`.`subsubcapitol`
+CHANGE COLUMN `subcapitol_id` `sub_capitol_id` INT(11) NOT NULL COMMENT '' ;
+ALTER TABLE `primarydb`.`subsubcapitol`
+ADD CONSTRAINT `sub_capitol_id`
+FOREIGN KEY (`sub_capitol_id`)
+REFERENCES `primarydb`.`subcapitol` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+CREATE TABLE `primarydb`.`comentarii` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '',
+  `comentarii` VARCHAR(1500) NULL COMMENT '',
+  `sub_sub_capitol_id` INT(11) NULL COMMENT '',
+  PRIMARY KEY (`id`)  COMMENT '');
+
+CREATE TABLE `primarydb`.`coding` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '',
+  `coding` VARCHAR(1500) NULL COMMENT '',
+  PRIMARY KEY (`id`)  COMMENT '');
+
+ALTER TABLE `primarydb`.`subsubcapitol`
+RENAME TO  `primarydb`.`sub_sub_capitol` ;
+
+ALTER TABLE `primarydb`.`subcapitol`
+RENAME TO  `primarydb`.`sub_capitol` ;
+
+ALTER TABLE `primarydb`.`content`
+DROP FOREIGN KEY `sub_sub_capitol_id`;
+
+ALTER TABLE `primarydb`.`sub_capitol`
+DROP FOREIGN KEY `capitol_id`;
+
+ALTER TABLE `primarydb`.`sub_sub_capitol`
+DROP FOREIGN KEY `sub_capitol_id`;
