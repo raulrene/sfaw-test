4f5c1239e129d7039afa46f05114ea100c5c38bf
Dan <dan.lepsa94@gmail.com>
2015-07-09 16:55:33 +0300

    created Courses page

--- config/config.php
+++ config/config.php
@@ -2,13 +2,14 @@
 $servername = "localhost";
 $username = "root";
 $password = "root";
+$db = 'newSite';
+
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password,$db);
 
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
 ?>
---
+++ course.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<?php
+include('config/config.php');
+?>
+
+<html>
+<head>
+    <?php
+    include('includes/head.php');
+    ?>
+</head>
+<body>
+<?php
+include('includes/header.php');
+?>
+<div class="site-wrap">
+    <div class="main-content">
+        <div class="row">
+            <?php
+            include('courses-loop.php');
+            ?>
+        </div>
+    </div>
+    <?php include('includes/footer.php'); ?>
+</div>
+</body>
+</html>
