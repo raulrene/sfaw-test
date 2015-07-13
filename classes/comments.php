dd922a4c41d612e46adbe3dc3b38ada4fb4a3b70
pelivansky <cpelivan30@yahoo.com>
2015-07-13 10:15:13 +0300

    modif class comments

--- classes/comments.php
+++ classes/comments.php
@@ -1,6 +1,6 @@
 <?php
 class Comments{
-    private $id, $content_id, $comments_content, $date, $tag;
+    private $id, $author, $author_img, $author_link,$date_posted, $comm_text;
     private $table = 'comments';
 
 
@@ -12,56 +12,89 @@ class Comments{
     }
     public function getCommentsById($id){
         global $db;
-        $q = 'SELECT * FROM $this->table WHERE id =$id';
+        $q = 'SELECT * FROM $this->table WHERE id = $id';
         $data = $db->fetch_row($q);
         return $data;
     }
+
     /**
-     * @param mixed $comments_content
+     * @param mixed $author
      */
-    public function setCommentsContent($comments_content)
+    public function setAuthor($author)
     {
-        $this->comments_content = $comments_content;
+        $this->author = $author;
     }
 
     /**
      * @return mixed
      */
-    public function getCommentsContent()
+    public function getAuthor()
     {
-        return $this->comments_content;
+        return $this->author;
     }
 
     /**
-     * @param mixed $content_id
+     * @param mixed $author_img
      */
-    public function setContentId($content_id)
+    public function setAuthorImg($author_img)
     {
-        $this->content_id = $content_id;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getContentId()
-    {
-        return $this->content_id;
-    }
-
-    /**
-     * @param mixed $date
-     */
-    public function setDate($date)
-    {
-        $this->date = $date;
+        $this->author_img = $author_img;
     }
 
     /**
      * @return mixed
      */
-    public function getDate()
+    public function getAuthorImg()
     {
-        return $this->date;
+        return $this->author_img;
+    }
+
+    /**
+     * @param mixed $author_link
+     */
+    public function setAuthorLink($author_link)
+    {
+        $this->author_link = $author_link;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthorLink()
+    {
+        return $this->author_link;
+    }
+
+    /**
+     * @param mixed $comm_text
+     */
+    public function setCommText($comm_text)
+    {
+        $this->comm_text = $comm_text;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCommText()
+    {
+        return $this->comm_text;
+    }
+
+    /**
+     * @param mixed $date_posted
+     */
+    public function setDatePosted($date_posted)
+    {
+        $this->date_posted = $date_posted;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDatePosted()
+    {
+        return $this->date_posted;
     }
 
     /**
@@ -81,20 +114,22 @@ class Comments{
     }
 
     /**
-     * @param mixed $tag
+     * @param string $table
      */
-    public function setTag($tag)
+    public function setTable($table)
     {
-        $this->tag = $tag;
+        $this->table = $table;
     }
 
     /**
-     * @return mixed
+     * @return string
      */
-    public function getTag()
+    public function getTable()
     {
-        return $this->tag;
+        return $this->table;
     }
+
+
 
 
 }
