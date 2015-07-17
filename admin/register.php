73ea35f1a94abfab47d57887f2f7e4e70da16f31
pelivansky <cpelivan30@yahoo.com>
2015-07-17 19:00:55 +0300

    register file

--- admin/register.php
+++ admin/register.php
@@ -11,6 +11,7 @@
     <link href="../assets/css/footer.css" type="text/css" rel="stylesheet">
 </head>
 <body>
+
 <div style="background: url('../assets/img/bck.jpg');width:100%;height:700px">
     <div style="border: 1px dotted #000;width:230px;padding:30px;position:relative;top:100px;left:300px">
         <h1>Register</h1>
@@ -35,10 +36,49 @@
             <input type="submit" name="submit" value="Push" class="fg-button teal"/>
         </form>
     </div>
-
 </div>
 
 </body>
 
 </html>
+<?php
+include_once '../connection.php';
+if(isset($_POST)){
+    if( !empty($_POST['name']) &&
+        !empty($_POST['email']) &&
+        !empty($_POST['phone']) &&
+        !empty($_POST['pass']) &&
+        !empty($_POST['pass_2']) ){
+            function clean_data($data) {
+                $data = trim($data);
+                $data = stripslashes($data);
+                $data = htmlspecialchars($data);
+                return $data;
+            }
+            $name    = clean_data($_POST['name']);
+            $email   = clean_data($_POST['email']);
+            $phone   = clean_data($_POST['phone']);
+            $pass    = clean_data($_POST['pass']);
+            $pass_2  = clean_data($_POST['pass_2']);
+
+            if($pass == $pass_2){
+                $pass = sha1($pass);
+
+                $q = "INSERT INTO users (name, password, email, phone)
+                      VALUES ('$name', '$pass', '$email', '$phone')
+                     ";
+                if($conn->query($q) == TRUE){
+                    $msg = 'added success now please..';
+                }else{
+                    echo "Error: " . $q . "<br>" . $conn->error;
+                }
+            }else{
+                echo 'passwords must match';
+            }
+    }else{
+        echo 'please fill in fields !';
+    }
+}
+?>
+
 
