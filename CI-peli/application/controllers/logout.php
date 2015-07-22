e0a29983c1e67879613b6189ff847890deaa7c14
pelivansky <cpelivan30@yahoo.com>
2015-07-22 12:11:14 +0300

    admin and logout controllers

---
+++ CI-peli/application/controllers/logout.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Logout extends CI_Controller {
+
+	public function index()	{
+
+		$this->session->unset_userdata();
+		$this->session->sess_destroy();
+		redirect('login');
+	
+	}
+
+}
+
