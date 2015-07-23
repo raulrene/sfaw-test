9f6458fd2d93fa71dd1de73d94a552adab388c0f
Radu Spinean <radu.spinean@yahoo.ro>
2015-07-23 10:48:37 +0300

    codeigniter

--- ci/ci/application/views/includes/site_head.php
+++
@@ -1,9 +0,0 @@
-﻿	<meta charset = "utf-8 without BOM"/>
-	<title>Academy-Learning</title>
-	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css" type="text/css"/>
-	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"/>	
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]--> 	
-	
